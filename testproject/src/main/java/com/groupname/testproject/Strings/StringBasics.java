634463da6913e066e1b1b18ef028b91de8ce50ac
Sunil Nagula <nagulasunilsidhu@gmail.com>
2022-07-29 21:42:23 +0530

    String basic

---
+++ testproject/src/main/java/com/groupname/testproject/Strings/StringBasics.java
@@ -0,0 +1,34 @@
+package com.groupname.testproject.Strings;
+
+public class StringBasics {
+
+	String name = "ajay"; // object creation using String literal
+	String dsgn = new String("ajay"); // object creation using new keyword
+	
+	
+	public void getString () {
+		
+		String surname = "java";
+		String name = "engineer";
+		
+		String concate = surname.concat(name);
+		String uppercase = surname.toUpperCase();
+		int comapre = surname.compareTo("java"); // 0 -> true , 1 - > false
+		
+		boolean equals = surname.equals("java");
+		boolean equalignore = surname.equalsIgnoreCase("java");
+		
+		
+		System.out.println(equalignore);
+	}
+	
+	
+	public  static void main(String args[]) {
+		
+		StringBasics basic = new StringBasics();
+		basic.getString();
+		
+	}
+	
+	
+}
